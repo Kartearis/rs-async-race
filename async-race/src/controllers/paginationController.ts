2897329c757fb0496072b6a558e4a418f9bc526a
Matveev Timofey <justshowel@gmail.com>
2022-08-08 23:36:40 +0300

    feat: Partially implement garageView. Implement paginationController.

---
+++ async-race/src/controllers/paginationController.ts
@@ -0,0 +1,77 @@
+
+
+export enum EventTypes {
+  pageChange = 'change',
+  totalChange = 'totalChange'
+}
+
+export type Handler = (currentPage?: number, totalPages?: number) => void;
+
+export default class PaginationController {
+
+  #pageNumber: number
+  #totalPages: number
+  #handlers: {
+    change: Handler[],
+    totalChange: Handler[],
+    [key: string]: Handler[]
+  }
+
+  // Pages are indexed from 1
+  constructor(totalPages: number = 1) {
+    this.#pageNumber = 1;
+    this.#totalPages = totalPages;
+    this.#handlers = {
+      change: [],
+      totalChange: []
+    };
+  }
+
+  set totalPages(total: number) {
+    if (total === this.#totalPages) return;
+    this.#totalPages = total;
+    this.#handlers.totalChange.forEach((handler: Handler) => handler(this.#pageNumber, this.#totalPages));
+  }
+
+  get totalPages(): number {
+    return this.#totalPages;
+  }
+
+  set pageNumber(page: number) {
+    if (page >= 1 && page <= this.#totalPages) {
+      this.#pageNumber = page;
+      this.#handlers.change.forEach((handler: Handler) => handler(this.#pageNumber, this.#totalPages));
+    }
+  }
+
+  get pageNumber(): number {
+    return this.#pageNumber;
+  }
+
+  addHandler(eventType: EventTypes, handler: Handler): void {
+    if (!this.#handlers[eventType].includes(handler))
+      this.#handlers[eventType].push(handler);
+  }
+
+  next(): void {
+    if (this.#pageNumber < this.#totalPages) {
+      this.#pageNumber += 1;
+      this.#handlers.change.forEach((handler: Handler) => handler(this.#pageNumber, this.#totalPages));
+    }
+  }
+
+  previous(): void {
+    if (this.#pageNumber > 1) {
+      this.#pageNumber -= 1;
+      this.#handlers.change.forEach((handler: Handler) => handler(this.#pageNumber, this.#totalPages));
+    }
+  }
+
+  goto(page: number) {
+    if (page === this.#pageNumber) return;
+    if (page >= 1 && page <= this.#totalPages) {
+      this.#pageNumber = page;
+      this.#handlers.change.forEach((handler: Handler) => handler(this.#pageNumber, this.#totalPages));
+    }
+  }
+}
--- async-race/src/views/garageView.ts
+++ async-race/src/views/garageView.ts
@@ -1,13 +1,91 @@
+import PaginationController, { EventTypes } from "../controllers/paginationController";
+import RequestController, { CarData, CarListData } from "../controllers/requestController";
+import { assertDefined } from "../components/usefulFunctions";
+import './garage-view.css';
 
+const template = `
+  <section class="setup-form">
+    Here is setup form
+  </section>
+  <section class="garage">
+    <h3>Garage <span id="totalCars">1</span></h3>
+<!--    <h5>Page #<span id="pageNum">1</span></h5>-->
+    <div class="garage__car-container"></div>
+    <div class="garage__pagination-container">
+      <button disabled class="garage__pagination-button" data-page="1" data-first><<</button>
+      <button disabled class="garage__pagination-button" data-direction="prev"><</button>
+      <button disabled class="garage__pagination-number" data-page="1">1</button>
+      <button disabled class="garage__pagination-button" data-direction="next">></button>
+      <button disabled class="garage__pagination-button" data-page="1" data-last>>></button>
+    </div>
+  </section>
+`;
 
 export default class GarageView {
   #rootElement: HTMLElement
+  #paginationController: PaginationController
+  #requestController: RequestController
+
 
   constructor(element: HTMLElement) {
     this.#rootElement = element;
+    this.#paginationController = new PaginationController();
+    this.#requestController = new RequestController('http://127.0.0.1:3000');
   }
 
   show(): void {
-    this.#rootElement.innerText = "Garage view";
+    this.#rootElement.innerHTML = template;
+    this.setupHandlers();
+    this.fillData(1).then(() => {
+      this.#paginationController.pageNumber = 1;
+    });
+  }
+
+  setupHandlers(): void {
+    const paginationContainer: HTMLElement = assertDefined(this.#rootElement.querySelector('.garage__pagination-container'));
+    paginationContainer
+      .addEventListener('click', (event: Event) => {
+        if (event.target instanceof  HTMLElement)
+          if (event.target.dataset['direction'] === 'prev') this.#paginationController.previous();
+          else if (event.target.dataset['direction'] === 'next') this.#paginationController.next();
+          else if (event.target.dataset['page'] !== undefined) this.#paginationController.goto(parseInt(event.target.dataset['page']));
+      });
+    const prevButton: HTMLButtonElement = assertDefined(paginationContainer.querySelector('[data-direction="prev"]'));
+    const firstButton: HTMLButtonElement = assertDefined(paginationContainer.querySelector('[data-first]'));
+    const pageNumber: HTMLButtonElement = assertDefined(paginationContainer.querySelector('.garage__pagination-number'));
+    const lastButton: HTMLButtonElement = assertDefined(paginationContainer.querySelector('[data-last]'));
+    const nextButton: HTMLButtonElement = assertDefined(paginationContainer.querySelector('[data-direction="next"]'));
+    this.#paginationController.addHandler(EventTypes.pageChange, (currentPage?: number, totalPages?: number) => {
+      this.fillData(assertDefined(currentPage));
+      prevButton.disabled = true;
+      nextButton.disabled = true;
+      firstButton.disabled = true;
+      lastButton.disabled = true;
+      if (currentPage !== 1) {
+        prevButton.disabled = false;
+        firstButton.disabled = false;
+      }
+      if (assertDefined(currentPage) < assertDefined(totalPages)) {
+        nextButton.disabled = false;
+        lastButton.disabled = false;
+      }
+      pageNumber.innerText = pageNumber.dataset['page'] = assertDefined(currentPage).toString();
+    });
+    this.#paginationController.addHandler(EventTypes.totalChange, (currentPage?: number, totalPages?: number) => {
+      lastButton.dataset['page'] = assertDefined(totalPages).toString();
+    });
+  }
+
+  async fillData(currentPage: number | null): Promise<void> {
+    const cars: CarListData = await this.#requestController.getCars(2, currentPage);
+    this.#paginationController.totalPages = cars.totalCars / 2;
+    const carContainer: HTMLElement = assertDefined(this.#rootElement.querySelector('.garage__car-container'));
+    carContainer.innerHTML = "";
+    console.log("Data filled");
+    cars.carList.forEach((car: CarData) => {
+      const tmp = document.createElement('div');
+      tmp.innerText = car.name + car.color;
+      carContainer.append(tmp);
+    });
   }
 }
